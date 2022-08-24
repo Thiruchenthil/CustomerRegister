ee0a360997585f55c3e16f0a8b356553954c8af1
Thiruchenthil <jthiru@gmail.com>
2022-08-24 13:31:33 -0400

    Database scripts updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,19 @@
-
- CREATE TABLE REGISTER(c_name varchar(20),
+DROP TABLE TESTING.REGISTER;
+ 
+CREATE TABLE TESTING.REGISTER(
+  c_c2code INTEGER PRIMARY KEY,
+ c_name varchar(20),
  d_dob date,
- c_mobile_no INTEGER PRIMARY KEY auto_increment,
+ c_mobile_no VARCHAR(20) ,
 c_email varchar(20),
-c_c2code INTEGER,
 c_gender char(10),
-c_pincode INTEGER,
+c_pincode VARCHAR(10),
 n_firm_id INTEGER, 
 c_br_code INTEGER, 
-c_service_active_status char(200));
-CREATE TABLE LOGINPAGE(c_mobile_no INTEGER PRIMARY KEY auto_increment,
+c_service_active_status VARCHAR(200));
+
+CREATE TABLE TESTING.LOGINPAGE(c_mobile_no INTEGER PRIMARY KEY auto_increment,
 OTP INTEGER,
 c_c2code INTEGER,
 n_login INTEGER);
-
 
