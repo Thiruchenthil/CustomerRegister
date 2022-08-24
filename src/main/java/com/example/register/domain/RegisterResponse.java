e20ccc5371fa0db8bdd2918c7a594c7d9220b606
Thiruchenthil <jthiru@gmail.com>
2022-08-24 13:23:21 -0400

    Added Resonse

---
+++ src/main/java/com/example/register/domain/RegisterResponse.java
@@ -0,0 +1,14 @@
+package com.example.register.domain;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterResponse implements Serializable{
+
+	private String response;
+	
+}
--- src/main/java/com/example/register/jpa/repo/LoginpageRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.register.jpa.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface LoginpageRepo extends CrudRepository<Loginpage, Integer> {
-
-}
